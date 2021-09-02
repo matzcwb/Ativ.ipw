6af1640581bf4e83c6dc84bad8e56f67fbb7d6c1
matzcwb <88168883+matzcwb@users.noreply.github.com>
2021-09-02 01:34:18 -0300

    Add files via upload

--- Estrutura de Repetição/Script4.js
+++ Estrutura de Repetição/Script4.js
@@ -1,4 +1,4 @@
-var impar = 0;
+
 for (var index = 1; index <= 20; index++){
    if (index % 2 == 0){
    alert(index)
--- Estrutura de Repetição/Script5.js
+++ Estrutura de Repetição/Script5.js
@@ -1,10 +1,12 @@
-
+quant = 0
 var par = 0
 for(i = 1 ;i <= 20; i++) {
    if (i % 2 == 0){
+       i
        
 
    
 
     }
 }
+alert(
