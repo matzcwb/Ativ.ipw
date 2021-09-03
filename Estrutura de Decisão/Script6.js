b347bcfe7a5706aaa80b37aefa396ae67e83b473
matzcwb <88168883+matzcwb@users.noreply.github.com>
2021-09-03 13:00:07 -0300

    Add files via upload

--- Estrutura de Decisão/Script6.js
+++ Estrutura de Decisão/Script6.js
@@ -1,11 +1,16 @@
-soma = 0;    
-    var numStr = prompt("Digite um nº inteiro de 4 dig: ");{
-    var aux = parseInt(numStr);
-    for(aux / 10;
-    soma %= aux
+var resto = 0;
+var soma = 0; 
+   var numStr = prompt("Digite um nº inteiro de 4 dig: ");{
+   var aux = parseInt(numStr);
 
+while (aux > 0){
 
-    } 
+resto = aux % 10;
+aux = (aux-resto)/10;
+soma = soma + resto;
 }
-    
-    
+}
+
+alert(soma)
+
+
