8f84466a5800e6af56e93ffee0f8e19fb1345e8a
matzcwb <88168883+matzcwb@users.noreply.github.com>
2021-09-11 14:06:13 -0300

    Add files via upload

---
+++ Estrutura de Repetição/Script9.js
@@ -0,0 +1,8 @@
+var numerador = -1;
+var resposta = 0;
+for (denominador = 1; denominador <= 50; denominador++){
+    numerador = numerador + 2;
+    resposta = resposta + (numerador / denominador);
+
+}
+alert(resposta)
